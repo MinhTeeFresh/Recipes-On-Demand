780189ae5807833491d07b13c6a251b206d6995d
najaf2 <najafali@ou.edu>
2022-03-03 17:21:22 -0600

    make skeleton for GET request

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
+// Require all our libraries including express
+// which will be used for our backend
 const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const fs = require("fs");
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
+// Read recipies
+const favs = require("/src/recipes/recipes.json");
+
+// Use the body parser so we can read incoming json
+app.use(bodyParser.json())
+
+// Use cors to deal with security and middleware
+app.use(cors({origin: '*'}));
+
+// load html home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+// return recipes depending on recieved ingredient
+app.get('/get', function (req, res) {
+  let ingredients = req.body
+
+  let listOfRecipies = []
+  let missSpelledList = []
+
+  // TODO algo to cross reference given ingredients to
+  // recipies
+
+  // TODO add miss spelled ingredients to list
+
+  // TODO return list of recipies and miss spelled list
+  res.json({
+    missSpelled: missSpelledList,
+    recipies: listOfRecipies
+  })
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
