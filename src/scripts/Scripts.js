d21f4cc8a64e47986bb7b7e76e208e36ab49cf74
Minh Tran <minh.h.tran-2@ou.edu>
2022-04-05 15:27:10 -0500

    Scripts.js for field filling

--- src/scripts/Scripts.js
+++ src/scripts/Scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         let ingredients = $("#userFill").val();
 		
 		$.ajax({
-			url: "/getRecognizedAndFailed",
+			url: "/getAll",
 			method: 'POST',
 			data: JSON.stringify({ ingredientsString : ingredients }),
 			contentType: 'application/json',
@@ -18,6 +18,54 @@ $(document).ready(function() {
 				
 				alert(`Matches: ${response.matched}`);
 				alert(`Fails: ${response.failed}`);
+
+				var error = $('#errorIngredients');
+				var recipeIngredients = $('#recipeIngredients');
+				var groceryList = $('#groceryList');
+				var image = $('#image');
+				var steps = $('#steps');
+				var name = $('#recipeTitle');
+
+				// Clearing out all previous entries
+                error.html('');
+				recipeIngredients.html('');
+				groceryList.html('');
+				image.html('');
+				steps.html('');
+				name.html('');
+
+				// Filling/Appending the matched ingredients
+				let data = '';
+				for(let i = 0; i < response.matched.length; i++) {
+					data += '<li>' + response.matched[i] + '</li>';
+				}
+                recipeIngredients.append(data);
+
+				// Filling in all error/failed entries
+				data = '';
+				if (response.failed[0] != null) {
+					data += response.failed[0];
+				 }
+				for(let i = 1; i < response.failed.length; i++) {
+					data += ', ' + response.failed[i];
+				}
+                error.append(data);
+
+				// Entering name into box
+				name.append(response.recipe.name);
+
+				// Filling in recipe steps
+				steps.append(response.recipe.steps);
+
+				// Filling in all unmatched entries
+				data = '';
+				for(let i = 0; i < response.unmatched.length; i++) {
+					data += '<li>' + response.unmatched[i] + '</li>';
+				}
+                groceryList.append(data);
+
+				// Inserting the image
+				
 			}
 		});
 
